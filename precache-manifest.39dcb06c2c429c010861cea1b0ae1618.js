9c56f241af85f69947948e1ea277fd458e0ccb6c
Lakshyajit <lakshyajit165@gmail.com>
2020-11-08 21:21:10 +0530

    Updates

---
+++ precache-manifest.39dcb06c2c429c010861cea1b0ae1618.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356c6da8713eec28d2d875d886d77f4d",
+    "url": "/polls/index.html"
+  },
+  {
+    "revision": "26f6bad75f24be8308bc",
+    "url": "/polls/static/css/2.f382c575.chunk.css"
+  },
+  {
+    "revision": "e2c5a5cd279cbbe6f12c",
+    "url": "/polls/static/css/main.1ddac806.chunk.css"
+  },
+  {
+    "revision": "26f6bad75f24be8308bc",
+    "url": "/polls/static/js/2.32ac92ea.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/polls/static/js/2.32ac92ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c5a5cd279cbbe6f12c",
+    "url": "/polls/static/js/main.70133ab9.chunk.js"
+  },
+  {
+    "revision": "b18eb610d3d75bb115d3",
+    "url": "/polls/static/js/runtime-main.0a44d519.js"
+  },
+  {
+    "revision": "1bc024be719c84b55d6b2fd1b8d4743d",
+    "url": "/polls/static/media/poll.1bc024be.svg"
+  }
+]);
